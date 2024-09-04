32618043bcbd794095b6341aae6a7252f7abbcd3
manideep1428 <saimanideep.ch12345@gmail.com>
2024-09-04 15:29:26 +0530

    Fixed

--- frontend/src/components/FileUploader.tsx
+++ frontend/src/components/FileUploader.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useState, useRef } from "react"
 import axios from "axios"
 import { Button } from "@/components/ui/button"
--- frontend/src/pages/RegisterForm.tsx
+++ frontend/src/pages/RegisterForm.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
